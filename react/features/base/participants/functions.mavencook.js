4c7d76c16238aea2703c3cef41655829df71dbb7
Pierre Reimertz <pierre.reimertz@gmail.com>
2018-04-04 17:12:06 +0200

    make sure we use Fellow Mavencooker everywhere :)

---
+++ react/features/base/participants/functions.mavencook.js
@@ -0,0 +1,50 @@
+// @flow
+
+import {
+    DEFAULT_AVATAR_RELATIVE_PATH,
+    LOCAL_PARTICIPANT_DEFAULT_ID
+} from './constants';
+
+import {
+    getAvatarURL,
+    getAvatarURLByParticipantId,
+    getLocalParticipant,
+    getParticipantById,
+    getParticipantCount,
+    getParticipants,
+    getPinnedParticipant,
+} from './functions'
+
+function getParticipantDisplayName(
+        stateful: Object | Function,
+        id: string) {
+    const participant = getParticipantById(stateful, id);
+
+    if (participant) {
+        if (participant.name) {
+            return participant.name;
+        }
+
+        if (participant.local) {
+            return typeof interfaceConfig === 'object'
+                ? interfaceConfig.DEFAULT_LOCAL_DISPLAY_NAME
+                : 'me';
+        }
+    }
+
+    return typeof interfaceConfig === 'object'
+        ? interfaceConfig.DEFAULT_REMOTE_DISPLAY_NAME
+        : 'Fellow Mavencooker';
+}
+
+export {
+    getParticipantDisplayName,
+
+    getAvatarURL,
+    getAvatarURLByParticipantId,
+    getLocalParticipant,
+    getParticipantById,
+    getParticipantCount,
+    getParticipants,
+    getPinnedParticipant,
+}
